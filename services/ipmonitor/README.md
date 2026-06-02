5a822d7b9c79bd1925632afd727b63b03ad801ed
oskolist <278026506+oskolist@users.noreply.github.com>
2026-06-02 09:55:39 +0330

    fix: now ipmonitor service works, and many service had minor fix too

--- services/ipmonitor/README.md
+++ services/ipmonitor/README.md
@@ -1,6 +1,11 @@
-# Status
-Not usable/Underconstruction
-
 # Intro
-The idea was to have a service which listen for public ip change and restart the services that need it(for example coturn or turn-rs)
+Some services like Coturn, IPFS, DDNS updater... needs know their own public IP or needs to be restarted when public IP address has changed. this service periodically checks public IP to fix this problem.
 
+# Status
+Seems working
+
+# TODO
+currenly it needs a static URL to curl to findout our public IP, but there are some alternates:
+- use a public STUN server
+- use upnp client
+- query IP address of own domain using DNS(and then confirm it it's really ours public IP)
--- services/kubo/container-startup.sh
+++ services/kubo/container-startup.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+case "${KUBO_IS_NETWORK_MODE_HOST-}" in
+    true|True|y|yes|1)
+	# https://github.com/ipfs/kubo/blob/master/bin/container_daemon
+	ipfs config Addresses.API "/ip4/${KUBO_API_BIND:-127.0.0.1}/tcp/${KUBO_API_PORT:-5001}"
+	ipfs config Addresses.Gateway "/ip4/${KUBO_GATEWAY_BIND:-127.0.0.1}/tcp/${KUBO_GATEWAY_PORT:-8080}"
+        ;;
+esac
+
 # Enable libp2p stream mounting
 if [ -n "${KUBO_LIBP2P_STREAM_MOUNTING-}" ]; then
   ipfs config --json Experimental.Libp2pStreamMounting "$KUBO_LIBP2P_STREAM_MOUNTING"
@@ -10,4 +18,4 @@ if [ -n "${KUBO_AUTOCONF_URL-}" ]; then
 fi
 
 # Add other configurations as needed
-# ipfs config --json Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001"]'
+#ipfs config --json Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001"]'
